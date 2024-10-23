e303f500cfe32ef04d5b0611b7cd47fff893c494
eiris elias <eiriselias@gmail.com>
2024-10-23 16:42:33 -0500

    se agrego el footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className='bg-primaryColor flex flex-col items-center text-white font-bold py-4'>
+      <div className='flex gap-8 mb-2'>
+        <Link href="https://www.linkedin.com/in/eiriselias" target="_blank">
+          <i className="fa-brands fa-linkedin text-4xl hover:opacity-80"></i>
+        </Link>
+        <Link href="https://www.github.com/eiriselias" target="_blank">
+          <i className="fa-brands fa-github text-4xl hover:opacity-80"></i>
+        </Link>
+        <Link href="https://www.facebook.com/eiriselias" target="_blank">
+          <i className="fa-brands fa-facebook text-4xl hover:opacity-80"></i>
+        </Link>
+      </div>
+      <div>
+        <p>© Diseñada por Eiris Salazar Full Stack Developer</p>
+      </div>
     </div>
   )
 }
