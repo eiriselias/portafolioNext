d403c2bbf43e47b86e5acde2116d5016c9691b62
eiris elias <eiriselias@gmail.com>
2024-10-23 14:05:13 -0500

    distribucion de nav, contenido y footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/components/navBar/NavBar";
+import Footer from "@/components/footer/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <header className="h-16">
+          <NavBar />
+        </header>
+        <main className="min-h-[85vh]">
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
