cc62d0b7780bbd3083737e1ff999fa72fb84e060
eiris elias <eiriselias@gmail.com>
2024-10-23 16:14:27 -0500

    se creo la primera vista del navBar

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div>
-      navBar
-    </div>
+    <nav className='flex justify-between px-16 font-bold bg-primaryColor text-white '>
+        <div className='flex items-center gap-2 hover:bg-selectColor px-4 hover:cursor-pointer'>
+            <i className="fa-duotone fa-solid fa-briefcase"></i>
+            <h2>Portafolio</h2>
+        </div>
+        <div>
+            <ul className='flex gap-2 hover:cursor-pointer'>
+                <li className='hover:bg-selectColor p-4'>
+                    <Link href="/">Principal</Link>
+                </li>
+                <li className='hover:bg-selectColor p-4'>
+                    <Link href="#">Habilidades</Link>
+                </li>
+                <li className='hover:bg-selectColor p-4'>
+                    <Link href="#">Proyectos</Link>
+                </li>
+                <li className='hover:bg-selectColor p-4'>
+                    <Link href="#">Certificados</Link>
+                </li>
+            </ul>
+        </div>
+        <div className='hover:bg-selectColor p-4 hover:cursor-pointer'>
+            <Link href="/">Iniciar Sesión</Link>
+        </div>
+    </nav>
   )
 }
 
