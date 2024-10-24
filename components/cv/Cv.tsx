767a6e9f2964d3f9876bc72d7e916dd9d32dccb6
Eiris Salazar <86639872+eiriselias@users.noreply.github.com>
2024-10-24 11:26:32 -0500

    componente cv

---
+++ components/cv/Cv.tsx
@@ -0,0 +1,13 @@
+const Cv = ()=>{
+  return(
+    <object
+      data=""
+      type="application/pdf"
+      width="600" height="400"
+    >
+       <p>Tu navegador no soporta PDF. Puedes <a href="ruta/al/archivo.pdf">descargar el PDF aquí</a>.</p>
+    </object>
+  )
+}
+
+export default Cv
