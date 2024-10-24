16137dacc1921edf6be1276e3a5c8d8de16639c9
Eiris Salazar <86639872+eiriselias@users.noreply.github.com>
2024-10-24 11:33:08 -0500

    Update Cv.tsx

--- components/cv/Cv.tsx
+++ components/cv/Cv.tsx
@@ -1,11 +1,11 @@
 const Cv = ()=>{
   return(
     <object
-      data=""
+      data="/1111776697.pdf"
       type="application/pdf"
       width="600" height="400"
     >
-       <p>Tu navegador no soporta PDF. Puedes <a href="ruta/al/archivo.pdf">descargar el PDF aquí</a>.</p>
+       <p>Tu navegador no soporta PDF. Puedes <a href="/1111776697.pdf">descargar el PDF aquí</a>.</p>
     </object>
   )
 }
