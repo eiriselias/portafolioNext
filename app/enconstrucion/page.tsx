71061c7d9f06b326c4435967e7eb86a5f787d89b
eiris elias <eiriselias@gmail.com>
2024-10-25 12:36:05 -0500

    modificacion2

--- app/enconstrucion/page.tsx
+++ app/enconstrucion/page.tsx
@@ -10,8 +10,8 @@ const EnconstrucionPage = () => {
     }
   return (
     <div className='flex flex-col justify-center items-center h-[80vh] relative'>
-      <img src="/images/Web-en-Construccion.png" alt="sitio en construccion" />
-      <Button onClick={handleClick}  className='absolute bottom-40'>Volver Al Inicio</Button>
+      <img src="/images/Web-en-Construccion.png" className='h-full' alt="sitio en construccion" />
+      <Button onClick={handleClick}  className='absolute bottom-4'>Volver Al Inicio</Button>
     </div>
   )
 }
