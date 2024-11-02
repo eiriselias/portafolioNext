15445d224a9633b18285a6eac2695ac3de312440
eiris elias <eiriselias@gmail.com>
2024-11-02 15:57:17 -0500

    se finalizo la pagina certificados

--- app/certificados/[id]/page.tsx
+++ app/certificados/[id]/page.tsx
@@ -1,10 +1,22 @@
 import Certificados from '@/components/certificados/Certificados'
 import React from 'react'
+import {certificadosData} from "@/helpers/certificados.helpers"
 
-const CertificadoIdPage = () => {
+const CertificadoIdPage:React.FC <{params:{id: string}}> = async ({params}) => {
+
+  const {id} = await params
+  const datas = await certificadosData
+
+  const dataCertificado = datas.find((data)=> data.id.toString() === id)
+
   return (
-    <Certificados/>
+    <>
+      {
+        dataCertificado && <Certificados {...dataCertificado}/>
+      }
+    </>
   )
+
 }
 
 export default CertificadoIdPage
