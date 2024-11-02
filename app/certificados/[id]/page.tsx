c314d4522f57fec046a03b4e387ccb1f92bd0542
Eiris Salazar <86639872+eiriselias@users.noreply.github.com>
2024-11-02 16:30:19 -0500

    Update page.tsx

--- app/certificados/[id]/page.tsx
+++ app/certificados/[id]/page.tsx
@@ -4,10 +4,9 @@ import {certificadosData, ICertificado} from "@/helpers/certificados.helpers"
 
 const CertificadoIdPage:React.FC <{params:{id: string}}> = async ({params}) => {
 
-  const {id} = await params
   const datas: ICertificado[] = certificadosData
 
-  const dataCertificado = datas.find((data)=> data.id.toString() === id)
+  const dataCertificado = datas.find((data)=> data.id === Number(params.id))
 
   return (
     <>
